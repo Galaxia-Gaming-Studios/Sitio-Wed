21cedc10280b2b2455c68ba14ac8cd5c681869c0
Galaxia-Gaming-Studios <comunidad18102010@gmail.com>
2024-09-15 12:01:19 -0600

    v1.0.0

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Mostrar una alerta cuando el sitio carga
+window.onload = function() {
+    alert('¡Bienvenido a mi sitio web estático!');
+};
+
+// Cambiar el color de fondo al hacer clic en un botón
+function cambiarColor() {
+    document.body.style.backgroundColor = 'lightblue';
+}
